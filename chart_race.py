bc2f5dae57c716584c716800ec9e509b3f3e3ec7
Ancentus <mancentus7@gmail.com>
2023-03-01 21:38:36 +0300

    Make horizontal scale fixed

--- chart_race.py
+++ chart_race.py
@@ -3,4 +3,4 @@ import pandas_alive
 
 elec_df = pd.read_csv("points_table.csv",index_col=0,parse_dates=[0])
 
-elec_df.fillna(0).plot_animated('pl_race.mp4',period_fmt="%Y-%m-%d",title='Premier League Race 2021-2022', enable_progress_bar=True)
+elec_df.fillna(0).plot_animated('pl_race.mp4',period_fmt="%Y-%m-%d",title='Premier League Race 2021-2022',fixed_max=True, enable_progress_bar=True)
